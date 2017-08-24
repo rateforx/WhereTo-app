da2d0d325e16359bed45b724360daef0a25ebfc8
Snippy <rateforx@gmail.com>
2017-08-24 15:19:19 +0200

    initial

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+let googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyC1tVc4lbElSKFHRRrngpVgB1lpkyD2qPo'
+});
 
-var app = express();
+let routes = require('./routes/index');
+let users = require('./routes/users');
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +22,7 @@ app.set('view engine', 'pug');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,10 +30,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,24 +41,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,25 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+// let router = express.Router();
+let app = express();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'pug-Bootstrap'});
+app.get('/', (req, res) => {
+// router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'WhereTo'
+    });
 });
 
-router.get('/about', (req, res, next) => {
-    res.render('about');
+app.get('/about', (req, res) => res.render('about'));
+
+app.get('/register', (req, res) => res.render('register'));
+app.post('/register', (req, res) => {
+    res.send(req.body);
 });
 
-router.get('/signup', (req, res, next) => {
-    res.render('signup', {});
-});
-
-router.get('/admin', (req, res, next) => {
+app.get('/admin', (req, res, next) => {
     res.sendStatus(401);
 });
 
-module.exports = router;
+// module.exports = router;
+module.exports = app;
